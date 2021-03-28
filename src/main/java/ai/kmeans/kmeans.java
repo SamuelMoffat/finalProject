f5b37d6b82f8c791c8aa5ae1e1a4072ddb9d3301
SamuelMoffat <sammoffat13@gmail.com>
2021-03-28 13:24:46 +0100

    implemented cluster distance comparison methods

--- src/main/java/ai/kmeans/kmeans.java
+++ src/main/java/ai/kmeans/kmeans.java
@@ -2,6 +2,7 @@ package main.java.ai.kmeans;
 
 import main.java.datageneration.DataPoint;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class kmeans {
@@ -10,11 +11,34 @@ public class kmeans {
     public static void findErrors(List<DataPoint> listOfPoints){
         //first select centroid data points
         double[] centroids = generateCentroids(listOfPoints);
-
+        System.out.println("CENTROIDS");
+        for(double centroid:centroids){
+            System.out.println(centroid);
+        }
+        generateClusters(centroids,listOfPoints);
     }
 
-    private static void generateClusters(){
+    private static void generateClusters(double[] centroids,List<DataPoint> listOfPoints ){
+        //compute distances
+        for(DataPoint dataPoint: listOfPoints){
+            double pressure = dataPoint.getPressure();
+            // get distance to centroids
+            BigDecimal distance1 = calculateDistance(centroids[0],pressure);
+            BigDecimal distance2 = calculateDistance(centroids[1],pressure);
 
+            System.out.println("Distance 1 = " + distance1);
+            System.out.println("Distance 2 = " + distance2);
+        }
+    }
+
+    private static BigDecimal calculateDistance(double centroidPressure, double dataPointPressure){
+        System.out.println(centroidPressure + " - " + dataPointPressure);
+        //convert to big decimal for precision
+        BigDecimal cPressure = BigDecimal.valueOf(centroidPressure);
+        BigDecimal dpPressure = BigDecimal.valueOf(dataPointPressure);
+        BigDecimal difference = cPressure.subtract(dpPressure);
+
+        return difference.abs();
     }
 
     private static double[] generateCentroids(List<DataPoint> listOfPoints){
